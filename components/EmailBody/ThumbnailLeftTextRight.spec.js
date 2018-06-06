3dfa4a3c41bff0fc77fe08f8fa2b0551a00ac13f
Xavier Julien <xjulien@octo.com>
2018-06-06 14:42:42 +0200

    Add tests for Thumbnails

---
+++ components/EmailBody/ThumbnailLeftTextRight.spec.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import ThumbnailLeftTextRight from '@/components/EmailBody/ThumbnailLeftTextRight'
+import configuration from '@/contents/config';
+
+jest.mock('@/contents/parisWeb.md', () => {
+    return require('@/components/spec/MarkdownMock')
+});
+
+describe('ThumbnailLeftTextRight Component', () => {
+    let $mounted;
+
+    const Constructor = Vue.extend(ThumbnailLeftTextRight);
+
+    beforeEach(() => {
+        $mounted = new Constructor({
+            propsData: {
+                title: configuration.parisWeb.title,
+                content: configuration.parisWeb.content,
+                image: configuration.parisWeb.image,
+                linkButton: configuration.parisWeb.linkButton
+            }
+        }).$mount()
+    });
+
+    test('snapshot', () => {
+        let $html = $mounted.$el.outerHTML;
+        expect($html).toMatchSnapshot()
+    });
+});
---
+++ components/EmailBody/ThumbnailRightTextLeft.spec.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import ThumbnailRightTextLeft from '@/components/EmailBody/ThumbnailRightTextLeft'
+import configuration from '@/contents/config';
+
+jest.mock('@/contents/parisWeb.md', () => {
+    return require('@/components/spec/MarkdownMock')
+});
+
+describe('ThumbnailRightTextLeft Component', () => {
+    let $mounted;
+
+    const Constructor = Vue.extend(ThumbnailRightTextLeft);
+
+    beforeEach(() => {
+        $mounted = new Constructor({
+            propsData: {
+                title: configuration.parisWeb.title,
+                content: configuration.parisWeb.content,
+                image: configuration.parisWeb.image,
+                linkButton: configuration.parisWeb.linkButton
+            }
+        }).$mount()
+    });
+
+    test('snapshot', () => {
+        let $html = $mounted.$el.outerHTML;
+        expect($html).toMatchSnapshot()
+    });
+});
