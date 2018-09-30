ad4a5ee1bd6f455fd1c93476c830c61a420d7067
Alexis Julien <ajulien@octo.com>
2018-09-30 22:08:53 +0200

    add vector for each technos logos with same size

--- components/EmailBody/NosMissions/Technos.vue
+++ components/EmailBody/NosMissions/Technos.vue
@@ -1,15 +1,17 @@
 <template>
     <div v-if="liste">
-        <img class="logo" src="@/assets/logos-techno/angularjs.png" v-if="liste.angularjs"/>
+        <img class="logo" src="@/assets/logos-techno/angularjs.svg" v-if="liste.angularjs"/>
         <img class="logo" src="@/assets/logos-techno/angular.svg" v-if="liste.angular"/>
-        <img class="logo" src="@/assets/logos-techno/react.png" v-if="liste.react"/>
-        <img class="logo" src="@/assets/logos-techno/vue.png" v-if="liste.vuejs"/>
-        <img class="logo" src="@/assets/logos-techno/nodejs.png" v-if="liste.nodejs"/>
-        <img class="logo" src="@/assets/logos-techno/java.png" v-if="liste.java"/>
-        <img class="logo" src="@/assets/logos-techno/ruby.png" v-if="liste.ruby"/>
-        <img class="logo" src="@/assets/logos-techno/backbone.png" v-if="liste.backbone"/>
-        <img class="logo" src="@/assets/logos-techno/ember.png" v-if="liste.ember"/>
-        <img class="logo" src="@/assets/logos-techno/python.png" v-if="liste.python"/>
+        <img class="logo" src="@/assets/logos-techno/react.svg" v-if="liste.react"/>
+        <img class="logo" src="@/assets/logos-techno/vuejs.svg" v-if="liste.vuejs"/>
+        <img class="logo" src="@/assets/logos-techno/nodejs.svg" v-if="liste.nodejs"/>
+        <img class="logo" src="@/assets/logos-techno/java.svg" v-if="liste.java"/>
+        <img class="logo" src="@/assets/logos-techno/ruby.svg" v-if="liste.ruby"/>
+        <img class="logo" src="@/assets/logos-techno/backbone.svg" v-if="liste.backbone"/>
+        <img class="logo" src="@/assets/logos-techno/ember.svg" v-if="liste.ember"/>
+        <img class="logo" src="@/assets/logos-techno/python.svg" v-if="liste.python"/>
+        <img class="logo" src="@/assets/logos-techno/graphql.svg" v-if="liste.graphql"/>
+        <img class="logo" src="@/assets/logos-techno/redux.svg" v-if="liste.redux"/>
     </div>
 </template>
 
