2ff6ea44526e7b1cd044fb1a46f5158b4ab608b5
Xavier Julien <xjulien@octo.com>
2018-10-08 23:44:58 +0200

    Move logo assets into Drive and not in assets

--- components/EmailBody/NosMissions/Technos.vue
+++ components/EmailBody/NosMissions/Technos.vue
@@ -1,27 +1,30 @@
 <template>
     <div v-if="liste">
-        <img class="logo" src="@/assets/logos-techno/angularjs.svg" title="AngularJS" v-if="liste.angularjs"/>
-        <img class="logo" src="@/assets/logos-techno/angular.svg" title="Angular" v-if="liste.angular"/>
-        <img class="logo" src="@/assets/logos-techno/react.svg" title="React" v-if="liste.react"/>
-        <img class="logo" src="@/assets/logos-techno/vuejs.svg" title="VueJS" v-if="liste.vuejs"/>
-        <img class="logo" src="@/assets/logos-techno/nodejs.svg" title="NodeJS" v-if="liste.nodejs"/>
-        <img class="logo" src="@/assets/logos-techno/java.svg" title="Java" v-if="liste.java"/>
-        <img class="logo" src="@/assets/logos-techno/ruby.svg" title="Ruby" v-if="liste.ruby"/>
-        <img class="logo" src="@/assets/logos-techno/backbone.svg" title="Backbone" v-if="liste.backbone"/>
-        <img class="logo" src="@/assets/logos-techno/ember.svg" title="Ember" v-if="liste.ember"/>
-        <img class="logo" src="@/assets/logos-techno/python.svg" title="Python" v-if="liste.python"/>
-        <img class="logo" src="@/assets/logos-techno/graphql.svg" title="GraphQL" v-if="liste.graphql"/>
-        <img class="logo" src="@/assets/logos-techno/redux.svg" title="Redux" v-if="liste.redux"/>
+        <img class="logo" :src="drive.linkDrive('18pZP0YmqNqGuBcsLqdlsoGISCXXY0LKk')" title="AngularJS" v-if="liste.angularjs"/>
+        <img class="logo" :src="drive.linkDrive('18mqlQZ3Vf93uSDCupPvlqyPzVWDy0FyB')" title="Angular" v-if="liste.angular"/>
+        <img class="logo" :src="drive.linkDrive('19VL3ajh_hBvb6b-UqXYpqpUn4ufFpO_x')" title="React" v-if="liste.react"/>
+        <img class="logo" :src="drive.linkDrive('19m6jT9t0XktYdyXJ0MyKQnAzsxSj5ddm')" title="VueJS" v-if="liste.vuejs"/>
+        <img class="logo" :src="drive.linkDrive('19FtL1rkGKb40tnYYd3iHp-v3hD_dVB2M')" title="NodeJS" v-if="liste.nodejs"/>
+        <img class="logo" :src="drive.linkDrive('19CGBZ-gx-nzVHh6aQBVsFxQ8wAgycjyv')" title="Java" v-if="liste.java"/>
+        <img class="logo" :src="drive.linkDrive('19eMOj4Axdmgt4Y9LqFpCbL5P7Fdzd9zj')" title="Ruby" v-if="liste.ruby"/>
+        <img class="logo" :src="drive.linkDrive('18v8jSlDj1PnTjB6GY_4ITbYAlzJka6sR')" title="Backbone" v-if="liste.backbone"/>
+        <img class="logo" :src="drive.linkDrive('192fsqTTXhroKaaJZwLxG6l2T0XZe6BTM')" title="Ember" v-if="liste.ember"/>
+        <img class="logo" :src="drive.linkDrive('19LtIQtWRhIbQGvUAQdMg5UBVquv54d5n')" title="Python" v-if="liste.python"/>
+        <img class="logo" :src="drive.linkDrive('195G-tmB6v0tJy9FYN5OC9LG-2ZN1kNsD')" title="GraphQL" v-if="liste.graphql"/>
+        <img class="logo" :src="drive.linkDrive('19X4sSUVX5DPzG9vQX5JJN96XzFkwN8sx')" title="Redux" v-if="liste.redux"/>
     </div>
 </template>
 
 <script>
+    import Drive from '@/services/linkDrive'
   export default {
     name: 'Technos',
     props: ['liste'],
 
     data () {
-      return {}
+      return {
+        drive: Drive
+      }
     },
 
     created () {
