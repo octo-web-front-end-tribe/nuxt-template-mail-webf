c1d1014228f29a4082d68f20b9e17d2658ff8b00
Xavier Julien <xjulien@octo.com>
2018-10-08 23:52:30 +0200

    Fix CSS for logo

--- components/EmailBody/NosMissions/Technos.vue
+++ components/EmailBody/NosMissions/Technos.vue
@@ -1,17 +1,17 @@
 <template>
     <div v-if="liste">
-        <img class="logo" :src="drive.linkDrive('18pZP0YmqNqGuBcsLqdlsoGISCXXY0LKk')" title="AngularJS" v-if="liste.angularjs"/>
-        <img class="logo" :src="drive.linkDrive('18mqlQZ3Vf93uSDCupPvlqyPzVWDy0FyB')" title="Angular" v-if="liste.angular"/>
-        <img class="logo" :src="drive.linkDrive('19VL3ajh_hBvb6b-UqXYpqpUn4ufFpO_x')" title="React" v-if="liste.react"/>
-        <img class="logo" :src="drive.linkDrive('19m6jT9t0XktYdyXJ0MyKQnAzsxSj5ddm')" title="VueJS" v-if="liste.vuejs"/>
-        <img class="logo" :src="drive.linkDrive('19FtL1rkGKb40tnYYd3iHp-v3hD_dVB2M')" title="NodeJS" v-if="liste.nodejs"/>
-        <img class="logo" :src="drive.linkDrive('19CGBZ-gx-nzVHh6aQBVsFxQ8wAgycjyv')" title="Java" v-if="liste.java"/>
-        <img class="logo" :src="drive.linkDrive('19eMOj4Axdmgt4Y9LqFpCbL5P7Fdzd9zj')" title="Ruby" v-if="liste.ruby"/>
-        <img class="logo" :src="drive.linkDrive('18v8jSlDj1PnTjB6GY_4ITbYAlzJka6sR')" title="Backbone" v-if="liste.backbone"/>
-        <img class="logo" :src="drive.linkDrive('192fsqTTXhroKaaJZwLxG6l2T0XZe6BTM')" title="Ember" v-if="liste.ember"/>
-        <img class="logo" :src="drive.linkDrive('19LtIQtWRhIbQGvUAQdMg5UBVquv54d5n')" title="Python" v-if="liste.python"/>
-        <img class="logo" :src="drive.linkDrive('195G-tmB6v0tJy9FYN5OC9LG-2ZN1kNsD')" title="GraphQL" v-if="liste.graphql"/>
-        <img class="logo" :src="drive.linkDrive('19X4sSUVX5DPzG9vQX5JJN96XzFkwN8sx')" title="Redux" v-if="liste.redux"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('18pZP0YmqNqGuBcsLqdlsoGISCXXY0LKk')" title="AngularJS" v-if="liste.angularjs"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('18mqlQZ3Vf93uSDCupPvlqyPzVWDy0FyB')" title="Angular" v-if="liste.angular"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('19VL3ajh_hBvb6b-UqXYpqpUn4ufFpO_x')" title="React" v-if="liste.react"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('19m6jT9t0XktYdyXJ0MyKQnAzsxSj5ddm')" title="VueJS" v-if="liste.vuejs"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('19FtL1rkGKb40tnYYd3iHp-v3hD_dVB2M')" title="NodeJS" v-if="liste.nodejs"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('19CGBZ-gx-nzVHh6aQBVsFxQ8wAgycjyv')" title="Java" v-if="liste.java"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('19eMOj4Axdmgt4Y9LqFpCbL5P7Fdzd9zj')" title="Ruby" v-if="liste.ruby"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('18v8jSlDj1PnTjB6GY_4ITbYAlzJka6sR')" title="Backbone" v-if="liste.backbone"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('192fsqTTXhroKaaJZwLxG6l2T0XZe6BTM')" title="Ember" v-if="liste.ember"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('19LtIQtWRhIbQGvUAQdMg5UBVquv54d5n')" title="Python" v-if="liste.python"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('195G-tmB6v0tJy9FYN5OC9LG-2ZN1kNsD')" title="GraphQL" v-if="liste.graphql"/>
+        <img style="max-width: 25px; margin-right: 10px;" :src="drive.linkDrive('19X4sSUVX5DPzG9vQX5JJN96XzFkwN8sx')" title="Redux" v-if="liste.redux"/>
     </div>
 </template>
 
@@ -35,8 +35,5 @@
 </script>
 
 <style scoped>
-    .logo {
-        max-width: 25px;
-        margin-right: 10px;
-    }
+    
 </style>
